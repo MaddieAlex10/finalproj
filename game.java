69e4b624636fd4455bdfdd7ffeb304deda1d4dfc
MaddieAlex10 <malexander30@leomail.tamuc.edu>
2026-04-26 13:54:20 -0500

    Update game.java

--- game.java
+++ game.java
@@ -1,1 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Random;
+
+public class RandomLine {
+    public static void main(String[] args) throws IOException {
+        // Read all lines into memory
+        List<String> lines = Files.readAllLines(Paths.get("yourfile.txt"));
+        
+        // Pick a random index
+        Random random = new Random();
+        String randomLine = lines.get(random.nextInt(lines.size()));
+        
+        System.out.println(randomLine);
+    }
+}
 
